7da4e591cbcd3a1f69f595c30945584033657533
wangmantao <wangmantao@163.com>
2018-07-15 22:04:44 +0800

    For new Fedora in win7 sys

--- spec/parse/project.clj
+++ spec/parse/project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :injections [(require 'spyscope.core)]
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compliment "0.3.6"]])
+                 [spyscope "0.1.6"]              ;; for debug
+                 [org.clojure/tools.trace "0.7.9"]
+                 [compliment "0.3.6"]])          ;; for complition
--- spec/parse/src/parse/core.clj
+++ spec/parse/src/parse/core.clj
@@ -1,11 +1,10 @@
 (ns parse.core
-  (:require [clojure.java.io :as io])
-  (:require [clojure.string :as s]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str])
+  ;;(:require [spyscope.core])
+  (:require [clojure.string :as s])
+  (require  [clojure.tools.trace :as trace]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
 
 (defn writeResource [name out_path]
   ;; name : io/resource (要导出的)
@@ -20,11 +19,52 @@
       (println "finish!")
 ))
 
-(defn tes []
-  ())
+
+  (defn indexable-words [text]
+    (let [indexable-word? (fn [w] (> (count w) 2))]    ;定义匿名函数, 并绑定给indexable-word?
+      (filter indexable-word? (s/split #"\\W+" text))))
+
+(defn a []
+  (indexable-words "wangMan")
+  )
+
+
 (defn main []
   ;;(writeResource "/home/wmt/test1.txt" "/home/wmt/test.txt")
   ;;(println "hi!"))
+  (println "读取文件打印每一行")
+  (with-open [rd (io/reader "/home/wmt/test.txt")]
+    (let [contents (line-seq rd)
+          order 65]  ;; order 65 -> A
+      (println "-----------")
+      (doseq [a contents]
+        (println a)
+        (if (= 0 (.length a))
+          (do
+            (println "新的一题开始"))
+            ;;(println order)(filter (fn [w] (> (count w) 2)) (re-split #"\\W+" "A fine day")) order reset
+            ;; 初始化新的变量)
+          (do
+            (if (s/starts-with? (s/trim a) "#")
+              (do
+                (let [qStr "&thinsp; <opt>aOrder</opt> oneOption"]
+                  ;;(println "有#号的是先项")
+                  ;;(println ( s/replace qStr "oneOption" (s/replace-first a "#" "")))
+                  (println ( s/replace (s/replace qStr "aOrder" (.toString (char 65))) "oneOption" (s/replace-first a "#" "")))
+                  )
+                )
+
+              (do
+                (let [ qStr "<p>( )<mtxt>qContent</mtxt></p>" ]
+                  ;;(println "没#号的是题目")
+                  (println ( s/replace qStr "qContent" a))
+                  )
+                )
+              )
+            ))))))
+
+
+  #_(
   (with-open [rd (io/reader "/home/wmt/test.txt")
               wr (io/writer "/home/wmt/test_new.txt")]
     (io/copy "wang mantao 汉字" wr))
